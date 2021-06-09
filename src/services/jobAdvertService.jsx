a83e0a9fcb6d006f33d3950620662c31e2e94f06
Batu Akdogan <77547523+batuakdogan@users.noreply.github.com>
2021-06-09 09:59:24 +0300

    Add files via upload

--- src/services/jobAdvertService.jsx
+++ src/services/jobAdvertService.jsx
@@ -1,9 +1,23 @@
 import axios from "axios";
 
-export default class JobAdvertService{
+export default class jobAdvertisementService{
     getJobAdverts(){
         return axios.get("http://localhost:8080/api/jobAdvertisements/getall")
     }
 
+    getAllActives(){
+        return axios.get("http://localhost:8080/api/jobAdvertisements/getallactive")
+    }
+
+    getAllActivesWithSorted(){  
+        return axios.get("http://localhost:8080/api/jobAdvertisements/getallactivesorted")
+    }
+
     
+    //Employer ID
+    getEmployerJobAds(id){
+        return axios.get("http://localhost:8080/api/jobAdvertisements/getEmployerJobAdvertisement?id="+id)
+    }
+  
+
 }
