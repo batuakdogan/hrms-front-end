36f76319803f65d4a4f7743f68e55da47d8f7750
Batu Akdogan <77547523+batuakdogan@users.noreply.github.com>
2021-07-19 23:18:13 +0300

    Delete workTypeService.jsx

--- src/services/workTypeService.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default class workTypeService{
-        getWorkTypes(){
-            return axios.get("http://localhost:8080/api/worktype/getall")
-        }
-    }
-
