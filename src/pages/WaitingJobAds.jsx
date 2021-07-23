0e7f7a1e9c7da0768bcd2f83fa630cba90b22ad3
Batu Akdogan <77547523+batuakdogan@users.noreply.github.com>
2021-07-23 09:08:33 +0000

    updated

--- src/pages/WaitingJobAds.jsx
+++ src/pages/WaitingJobAds.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Icon, Menu, Table, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import JobAdService from "../services/JobAdService";
 
 
@@ -24,94 +25,111 @@ useEffect(()=>{
     
    
 },[])
+const {authItem} = useSelector(state => state.auth)
 
 
 
     return (
+      
         <div>
+          {authItem[0].user.userType !==3 &&
+        <div className="ui negative message">
+          <div className="header">
+            Bu sayfayı görüntülemeye yetkiniz yok
+          </div>
+        </div>
+      }
            
-            <Table celled color={"black"}>
-        <Table.Header>
-          <Table.Row> 
-          <Table.HeaderCell>İş İlan ID</Table.HeaderCell>
+           {authItem[0].user.userType ===3 &&
+           
+           
+           
+           <Table celled color={"black"}>
+           <Table.Header>
+             <Table.Row> 
+               <Table.HeaderCell>Şirket Adı</Table.HeaderCell>
+               <Table.HeaderCell>Şehir</Table.HeaderCell>
+               <Table.HeaderCell>Maaş Aralığı</Table.HeaderCell>
+               <Table.HeaderCell>Çalışma Zamanı</Table.HeaderCell>
+               <Table.HeaderCell>Çalışma Yeri</Table.HeaderCell>
+               <Table.HeaderCell>Son Tarih</Table.HeaderCell>
+               <Table.HeaderCell>Detaylar</Table.HeaderCell>
+             </Table.Row>
+           </Table.Header>
+   
+           <Table.Body>
+             {
+               jobAds?.map(jobAd => (
+                 <Table.Row key={jobAd.id}>
+                              <Table.Cell>{jobAd.id}</Table.Cell>
+   
+                 <Table.Cell>{jobAd.employer.companyName}</Table.Cell>
+                 <Table.Cell>{jobAd.jobPosition.name}</Table.Cell>
+                 <Table.Cell>{jobAd.city.name}</Table.Cell>
+                 <Table.Cell>{jobAd.minSalary}₺ - {jobAd.maxSalary}₺</Table.Cell>
+                 <Table.Cell>{jobAd.workTime.name}</Table.Cell>
+                 <Table.Cell>{jobAd.workPlace.name}</Table.Cell>
+                 <Table.Cell>
+                   {(
+                     (new Date(jobAd.lastDate).getTime() -
+                       new Date(Date.now()).getTime()) /
+                     86400000
+                   )
+                     .toString()
+                     .split(".", 1)}{" "}
+                   gün
+                 </Table.Cell>
+                 <Table.Cell>
+               
+                   <Button as={Link} to={`/jobads/${jobAd.id}`}
+                       content="Detayları Gör"
+                       icon="right arrow"
+                       labelPosition="right"
+                     />
+                     
+                 </Table.Cell>
+               
+   
+                 </Table.Row>
+               ))}
+           </Table.Body>
+   
+           <Table.Footer>
+             <Table.Row>
+               <Table.HeaderCell colSpan="6">
+                 <Menu floated="right" pagination>
+                   <Menu.Item as="a" icon> 
+                     <Icon name="chevron left" />
+                   </Menu.Item>
+                   <Menu.Item as="a">1</Menu.Item>
+                   <Menu.Item as="a">2</Menu.Item>
+                   <Menu.Item as="a">3</Menu.Item>
+                   <Menu.Item as="a">4</Menu.Item>
+                   <Menu.Item as="a" icon>
+                     <Icon name="chevron right" />
+                   </Menu.Item>
+                 </Menu>
+               </Table.HeaderCell>
+             </Table.Row>
+           </Table.Footer>
+   
+         </Table>
 
-            <Table.HeaderCell>Şirket Adı</Table.HeaderCell>
-            <Table.HeaderCell>Pozisyon</Table.HeaderCell>
-            <Table.HeaderCell>Şehir</Table.HeaderCell>
-            <Table.HeaderCell>Maaş Aralığı</Table.HeaderCell>
-            <Table.HeaderCell>Çalışma Zamanı</Table.HeaderCell>
-            <Table.HeaderCell>Çalışma Yeri</Table.HeaderCell>
-            <Table.HeaderCell>Son Tarih</Table.HeaderCell>
-            <Table.HeaderCell>Detaylar</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {
-            jobAds?.map(jobAd => (
-              <Table.Row key={jobAd.id}>
-                           <Table.Cell>{jobAd.id}</Table.Cell>
-
-              <Table.Cell>{jobAd.employer.companyName}</Table.Cell>
-              <Table.Cell>{jobAd.jobPosition.name}</Table.Cell>
-              <Table.Cell>{jobAd.city.name}</Table.Cell>
-              <Table.Cell>{jobAd.minSalary}₺ - {jobAd.maxSalary}₺</Table.Cell>
-              <Table.Cell>{jobAd.workTime.name}</Table.Cell>
-              <Table.Cell>{jobAd.workPlace.name}</Table.Cell>
-              <Table.Cell>
-                {(
-                  (new Date(jobAd.lastDate).getTime() -
-                    new Date(Date.now()).getTime()) /
-                  86400000
-                )
-                  .toString()
-                  .split(".", 1)}{" "}
-                gün
-              </Table.Cell>
-              <Table.Cell>
-                <Button as={Link} to={`/jobads/${jobAd.id}`}
-                    content="Detayları Gör"
-                    icon="right arrow"
-                    labelPosition="right"
-                  />
-              </Table.Cell>
-            
-
-              </Table.Row>
-            ))}
-        </Table.Body>
-
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan="6">
-              <Menu floated="right" pagination>
-                <Menu.Item as="a" icon> 
-                  <Icon name="chevron left" />
-                </Menu.Item>
-                <Menu.Item as="a">1</Menu.Item>
-                <Menu.Item as="a">2</Menu.Item>
-                <Menu.Item as="a">3</Menu.Item>
-                <Menu.Item as="a">4</Menu.Item>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron right" />
-                </Menu.Item>
-              </Menu>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
-
-      </Table>
+           
+           }
+      {authItem[0].user.userType ===3  &&
       <Link to="/confirmAds">
-        <Button className="ui button">
+        <Button className="ui button" color="red">
           İş İlanı Onayla
         </Button> 
       </Link>
+    }    
 
-      
 
-      
+
         
         </div>
+      
     )
    
 }
